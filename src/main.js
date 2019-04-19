b072fe56264ce6faf1ddb352854318c7f2c24e97
gujiayi <gdgujiayi@126.com>
2019-04-19 15:32:52 +0800

    删除directives

--- src/main.js
+++ src/main.js
@@ -6,19 +6,16 @@ import router from './router'
 import store from './store'
 import iView from 'iview'
 import config from '@/config'
-import importDirective from '@/directive'
 import { directive as clickOutside } from 'v-click-outside-x'
 import installPlugin from '@/plugin'
 import './index.less'
 import '@/assets/icons/iconfont.css'
-import TreeTable from 'tree-table-vue'
 // 实际打包时应该不引入mock
 /* eslint-disable */
 if (process.env.NODE_ENV !== 'production') require('@/mock')
 
 Vue.use(iView, {
 })
-Vue.use(TreeTable)
 /**
  * @description 注册admin内置插件
  */
@@ -34,7 +31,6 @@ Vue.prototype.$config = config
 /**
  * 注册指令
  */
-importDirective(Vue)
 Vue.directive('clickOutside', clickOutside)
 
 /* eslint-disable no-new */
